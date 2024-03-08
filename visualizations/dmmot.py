6a3aaa3599cae9ce4a387b6aeb0fcb4302fbefe3
Ronak Mehta <ronakrm@cs.wisc.edu>
2024-03-08 18:19:09 -0500

    added a list version for varying length arrays

--- visualizations/dmmot.py
+++ visualizations/dmmot.py
@@ -59,7 +59,7 @@ def discrete_mmot(A, verbose=False, log=False):
         return obj
 
 
-def discrete_mmot_converge(A, first_fixed=False, niters=100, lr=0.1, print_rate=100, verbose=False, log=False):
+def discrete_mmot_converge(A, first_fixed=False, niters=100, lr=0.1, print_rate=100, verbose=False, log=False, sched_drop_time=250):
 
     d, n = A.shape
     def dualIter(A, lr):
@@ -95,7 +95,7 @@ def discrete_mmot_converge(A, first_fixed=False, niters=100, lr=0.1, print_rate=
 
     for i in range(niters):
 
-        if i > 250:
+        if i > sched_drop_time:
             lr = lr*0.995
         funcval, A, grad, _ = dualIter(A, lr)
 
@@ -126,6 +126,107 @@ def discrete_mmot_converge(A, first_fixed=False, niters=100, lr=0.1, print_rate=
     else:
         return A 
 
+def list_discrete_mmot(aa):
+
+    def OBJ(i):
+        return max(i) - min(i)
+        # return 0 if max(i) == min(i) else 1
+
+    sum_aa = [sum(_) for _ in aa]
+    #assert abs(max(sum_aa)-min(sum_aa)) < 1e-10
+    AA = [np.copy(_) for _ in aa]
+
+    dims = tuple([len(_) for _ in AA])
+    xx = {}
+    dual = [np.zeros(d) for d in dims]
+
+    idx = [0,]*len(AA)
+    obj = 0
+
+    while all([i < _ for _, i in zip(dims, idx)]):
+        vals = [v[i] for v, i in zip(AA, idx)]
+        minval = min(vals)
+        i = vals.index(minval)
+        xx[tuple(idx)] = minval
+        obj += (OBJ(idx)) * minval
+        for v, j in zip(AA, idx): v[j] -= minval
+        oldidx = np.copy(idx)
+        idx[i] += 1
+        if idx[i]<dims[i]:
+            dual[i][idx[i]] += OBJ(idx) - OBJ(oldidx) + dual[i][idx[i]-1]
+
+    # the above terminates when any entry in idx equals the corresponding value in dims
+    # this leaves other dimensions incomplete; the remaining terms of the dual solution 
+    # must be filled-in
+    for _, i in enumerate(idx):
+        try: dual[_][i:] = dual[_][i]
+        except: pass
+
+    dualobj =  sum([np.array(_).dot(np.array(_d)) for _, _d in zip(aa, dual)])
+    
+    return {'x': xx, 'primal objective': obj,
+            'dual': dual, 'dual objective': dualobj}
+
+# For differently sized inputs we can't use a single np array
+# this function is a list-based version of the above, which 
+# calls list_discrete_mmot so varying length inputs can be used
+def list_discrete_mmot_converge(aa, niters=100, lr=0.1, print_rate=100, verbose=False, log=False, sched_drop_time=250):
+
+    def dualIter(aa, lr):
+        log_dict = list_discrete_mmot(aa)
+        aa_new = [np.copy(_) for _ in aa]
+        grad = log_dict['dual']
+        for i in range(len(aa)):
+            aa_new[i] -= aa[i] * (grad[i]/len(aa[i])) * lr
+        return log_dict['primal objective'], aa_new, grad, log_dict
+
+    def renormalize(aa):
+        for i in range(len(aa)):
+            if min(aa[i]) < 0:
+                aa[i] -= min(aa[i])
+            s = sum(aa[i])
+            aa[i] = [aa[i][j]/s for j in range(len(aa[i]))]
+        return aa
+
+    funcval, _, grad, _ = dualIter(aa, lr)
+
+    if log:
+        logs = []
+        log_dict = {}
+        log_dict['A'] = renormalize(aa)
+        log_dict['func_val'] = funcval
+        logs.append(log_dict)
+
+    if verbose:
+        print(f'Inital:\t\tObj:\t{funcval:.4f}')
+
+    for i in range(niters):
+
+        if i > sched_drop_time:
+            lr = lr*0.995
+        funcval, aa, grad, _ = dualIter(aa, lr)
+
+        aa = renormalize(aa)
+
+        if i % print_rate == 0:
+            if verbose:
+                print(f'Iter {i:2.0f}:\tObj:\t{funcval:.4f}')
+            if log:
+                log_dict = {}
+                log_dict['A'] = renormalize(aa.copy())
+                log_dict['func_val'] = funcval
+                logs.append(log_dict)
+
+    if log:
+        log_dict = {}
+        log_dict['A'] = renormalize(aa)
+        log_dict['func_val'] = funcval
+        logs.append(log_dict)
+
+        return aa, logs
+    else:
+        return aa
+
 
 if __name__ == "__main__":
 
@@ -135,7 +236,16 @@ if __name__ == "__main__":
     A = np.vstack((a1,a2))
 
     funcval, log_dict = discrete_mmot(A, verbose=True, log=True)
-    print(a1)
-    print(a2)
 
-    print(log_dict)
+
+    a1 = [0.25,0.3,0.3,0.1,0.05]
+    a2 = [0.1,0.2,0.5,0.2]
+
+    A = [a1,a2]
+
+    # log_dict = list_discrete_mmot(A)
+    # print(log_dict)
+
+    A, logs = list_discrete_mmot_converge(A, niters=1000, verbose=True, log=True)
+    for a in A:
+        print(np.round(np.array(a),2))
